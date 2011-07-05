652a435f842aef252268ca9896abc4a7c58548a2
Cyril Mottier <cyril@cyrilmottier.com>
2011-07-05 22:14:14 +0200

    Add of some license headers

--- src/com/cyrilmottier/android/listviewtipsandtricks/HomeListActivity.java
+++ src/com/cyrilmottier/android/listviewtipsandtricks/HomeListActivity.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2011 Cyril Mottier (http://www.cyrilmottier.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.cyrilmottier.android.listviewtipsandtricks;
 
 import java.util.List;
@@ -20,28 +35,6 @@ import android.widget.ListView;
 public class HomeListActivity extends ListActivity {
 
     private static final String CATEGORY_SAMPLE_CODE = "com.cyrilmottier.android.listviewtipsandtricks.SAMPLE_CODE";
-
-    /**
-     * Represents a single ListView tip & trick in the list of all tips. A
-     * {@link Tip} is basically a title and an Intent to the Activity demo-ing
-     * the tip & trick
-     * 
-     * @author Cyril Mottier
-     */
-    private static class Tip {
-        public String title;
-        public Intent intent;
-
-        public Tip(String title, Intent intent) {
-            this.title = title;
-            this.intent = intent;
-        }
-
-        @Override
-        public String toString() {
-            return title;
-        }
-    }
 
     private ArrayAdapter<Tip> mAdapter;
 
@@ -84,6 +77,28 @@ public class HomeListActivity extends ListActivity {
         Tip tip = mAdapter.getItem(position);
         if (tip != null) {
             startActivity(tip.intent);
+        }
+    }
+    
+    /**
+     * Represents a single ListView tip & trick in the list of all tips. A
+     * {@link Tip} is basically a title and an Intent to the Activity demo-ing
+     * the tip & trick
+     * 
+     * @author Cyril Mottier
+     */
+    private static class Tip {
+        public String title;
+        public Intent intent;
+
+        public Tip(String title, Intent intent) {
+            this.title = title;
+            this.intent = intent;
+        }
+
+        @Override
+        public String toString() {
+            return title;
         }
     }
 
